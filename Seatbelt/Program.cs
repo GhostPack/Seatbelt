6fbe4ec67301f25fb23f9b6dfddd287197dbcbb5
swarleysez <31361532+swarleysez@users.noreply.github.com>
2019-01-15 10:43:18 -0500

    Added CrowdStrike Falcon Processes
    Added additional CrowdStrike Falcon processes (CSFalconContainer.exe and CSFalconService.exe)

--- Seatbelt/Program.cs
+++ Seatbelt/Program.cs
@@ -3295,6 +3295,8 @@ namespace Seatbelt
                 {"bds-vision.exe"              , "BDS Vision behavioral analysis"},
                 {"Triumfant.exe"               , "Triumfant behavioral analysis"},
                 {"CSFalcon.exe"                , "CrowdStrike Falcon EDR"},
+                {"CSFalconContainer.exe"       , "CrowdStrike Falcon EDR"},
+                {"CSFalconService.exe"         , "CrowdStrike Falcon EDR"},
                 {"ossec.exe"                   , "OSSEC intrusion detection"},
                 {"TmPfw.exe"                   , "Trend Micro firewall"},
                 {"dgagent.exe"                 , "Verdasys Digital Guardian DLP"},

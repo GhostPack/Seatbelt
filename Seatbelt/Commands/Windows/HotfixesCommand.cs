0e7672a193ffa1148f500987f25b3bb208e7a6c0
harmj0y <will@harmj0y.net>
2020-06-18 15:09:28 -0700

    Added Windows Hotfixes and Microsoft Update enumeration
    -Added Windows Hotfix ('Hotfixes') enumeration via the Win32_QuickFixEngineering WMI class
    -Added all Microsoft update enumeration ('MicrosoftUpdates') via the Microsoft.Update.Searcher COM object

---
+++ Seatbelt/Commands/Windows/HotfixesCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Reflection;
+using Seatbelt.Interop;
+using Seatbelt.Output.Formatters;
+using Seatbelt.Output.TextWriters;
+
+namespace Seatbelt.Commands.Windows
+{
+    internal class HotfixCommand : CommandBase
+    {
+        public override string Command => "Hotfixes";
+        public override string Description => "Installed hotfixes (via WMI)";
+        public override CommandGroup[] Group => new[] { CommandGroup.System, CommandGroup.Remote };
+        public override bool SupportRemote => true;
+        public Runtime ThisRunTime;
+
+        public HotfixCommand(Runtime runtime) : base(runtime)
+        {
+            ThisRunTime = runtime;
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            // Lists installed hotfixes via WMI (the Win32_QuickFixEngineering class)
+            // This is similar to PowerShell's Get-Hotfix
+            //  Note:   This class returns only the updates supplied by Component Based Servicing (CBS).
+            //          Updates supplied by Microsoft Windows Installer (MSI) or the Windows update site (https://update.microsoft.com) are not returned.
+            // Translation: this only shows (usually) _Windows_ updates, not all _Microsoft_ updates. For that use the "MicrosoftUpdates" command.
+
+            WriteHost("Enumerating Windows Hotfixes. For *all* Microsoft upates, use the 'MicrosoftUpdates' command.\r\n");
+
+            var wmiData = ThisRunTime.GetManagementObjectSearcher(@"root\cimv2", "SELECT * FROM Win32_QuickFixEngineering");
+            var data = wmiData.Get();
+            foreach (var hotfix in data)
+            {
+                yield return new HotfixDTO(
+                    hotfix["HotFixID"].ToString(),
+                    Convert.ToDateTime(hotfix["InstalledOn"].ToString()).ToUniversalTime(),
+                    hotfix["Description"].ToString(),
+                    hotfix["InstalledBy"].ToString()
+                );
+            }
+        }
+    }
+
+    internal class HotfixDTO : CommandDTOBase
+    {
+        public HotfixDTO(string hotFixID, DateTime? installedOnUTC, string description, string installedBy)
+        {
+            HotFixID = hotFixID;
+            InstalledOnUTC = installedOnUTC;
+            Description = description;
+            InstalledBy = installedBy;
+        }
+        public string HotFixID { get; set; }
+        public DateTime? InstalledOnUTC { get; set; }
+        public string Description { get; set; }
+        public string InstalledBy { get; set; }
+    }
+
+    [CommandOutputType(typeof(HotfixDTO))]
+    internal class HotfixTextFormatter : TextFormatterBase
+    {
+        public HotfixTextFormatter(ITextWriter writer) : base(writer)
+        {
+        }
+
+        public override void FormatResult(CommandBase? command, CommandDTOBase result, bool filterResults)
+        {
+            var dto = (HotfixDTO)result;
+
+            WriteLine($"  {dto.HotFixID,-10} {dto.InstalledOnUTC?.ToLocalTime(),-22} {dto.Description,-30} {dto.InstalledBy}");
+        }
+    }
+}
---
+++ Seatbelt/Commands/Windows/MicrosoftUpdatesCommand.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Seatbelt.Interop;
+using Seatbelt.Output.Formatters;
+using Seatbelt.Output.TextWriters;
+
+namespace Seatbelt.Commands.Windows
+{
+    internal class MicrosoftUpdateCommand : CommandBase
+    {
+        public override string Command => "MicrosoftUpdates";
+        public override string Description => "All Microsoft updates.";
+        public override CommandGroup[] Group => new[] { CommandGroup.Misc};
+        public override bool SupportRemote => false;
+
+        // TODO: remote? https://stackoverflow.com/questions/15786294/retrieve-windows-update-history-using-wuapilib-from-a-remote-machine
+
+        public MicrosoftUpdateCommand(Runtime runtime) : base(runtime)
+        {
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            WriteHost("Enumerating *all* Microsoft upates\r\n");
+
+            // oh how I hate COM...
+            var searcher = Type.GetTypeFromProgID("Microsoft.Update.Searcher");
+            var searcherObj = Activator.CreateInstance(searcher);
+
+            // get the total number of updates
+            var count = (int)searcherObj.GetType().InvokeMember("GetTotalHistoryCount", BindingFlags.InvokeMethod, null, searcherObj, new object[] { });
+
+            // get the pointer to the update collection
+            var results = searcherObj.GetType().InvokeMember("QueryHistory", BindingFlags.InvokeMethod, null, searcherObj, new object[] {0, count});
+
+            for(int i = 0; i < count; ++i)
+            {
+                // get the actual update item
+                var item = searcherObj.GetType().InvokeMember("Item", BindingFlags.GetProperty, null, results, new object[] { i });
+
+                // get our properties
+                //  ref - https://docs.microsoft.com/en-us/windows/win32/api/wuapi/nn-wuapi-iupdatehistoryentry
+                var title = searcherObj.GetType().InvokeMember("Title", BindingFlags.GetProperty, null, item, new object[] { }).ToString();
+                var date = searcherObj.GetType().InvokeMember("Date", BindingFlags.GetProperty, null, item, new object[] { });
+                var description = searcherObj.GetType().InvokeMember("Description", BindingFlags.GetProperty, null, item, new object[] { });
+                var clientApplicationID = searcherObj.GetType().InvokeMember("ClientApplicationID", BindingFlags.GetProperty, null, item, new object[] { });
+
+                string hotfixId = "";
+                Regex reg = new Regex(@"KB\d*");
+                var matches = reg.Matches(title);
+                if (matches.Count > 0)
+                {
+                    hotfixId = matches[0].ToString();
+                }
+
+                yield return new MicrosoftUpdateDTO(
+                    hotfixId,
+                    Convert.ToDateTime(date.ToString()).ToUniversalTime(),
+                    title,
+                    clientApplicationID.ToString(),
+                    description.ToString()
+                );
+            }
+        }
+    }
+
+    internal class MicrosoftUpdateDTO : CommandDTOBase
+    {
+        public MicrosoftUpdateDTO(string hotFixID, DateTime? installedOnUTC, string title, string dlientApplicationID, string description)
+        {
+            HotFixID = hotFixID;
+            InstalledOnUTC = installedOnUTC;
+            Title = title;
+            ClientApplicationID = dlientApplicationID;
+            Description = description;
+        }
+        public string HotFixID { get; set; }
+        public DateTime? InstalledOnUTC { get; set; }
+        public string Title { get; set; }
+        public string ClientApplicationID { get; set; }
+        public string Description { get; set; }
+    }
+
+    [CommandOutputType(typeof(MicrosoftUpdateDTO))]
+    internal class MicrosoftUpdateFormatter : TextFormatterBase
+    {
+        public MicrosoftUpdateFormatter(ITextWriter writer) : base(writer)
+        {
+        }
+
+        public override void FormatResult(CommandBase? command, CommandDTOBase result, bool filterResults)
+        {
+            var dto = (MicrosoftUpdateDTO)result;
+
+            WriteLine($"  {dto.HotFixID,-10} {dto.InstalledOnUTC?.ToLocalTime(),-23} {dto.ClientApplicationID,-20} {dto.Title}");
+        }
+    }
+}

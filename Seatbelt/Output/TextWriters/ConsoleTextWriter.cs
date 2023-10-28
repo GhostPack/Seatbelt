22a7789375206628a3140655df9439225ae0a382
bacon-jam <bacon-jam>
2023-10-27 23:17:37 -0400

    adding UTF8 as the default encoding for ConsoleTextWriter

--- Seatbelt/Output/TextWriters/ConsoleTextWriter.cs
+++ Seatbelt/Output/TextWriters/ConsoleTextWriter.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Text;
 
 namespace Seatbelt.Output.TextWriters
 {
     internal class ConsoleTextWriter : ITextWriter
     {
+        public ConsoleTextWriter() => Console.OutputEncoding = Encoding.UTF8;
         public void Write(string str)
             => Console.Write(str);
 
         public void WriteLine()
             => Console.WriteLine();
 
-        public void WriteLine(string str) 
+        public void WriteLine(string str)
             => Console.WriteLine(str);
 
         public void WriteLine(string format, params object?[] args) => Console.WriteLine(format, args);
